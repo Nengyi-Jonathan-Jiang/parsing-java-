4df531a0e2613f566c5186b7a035a5cba20347cf
Nengyi Jonathan Jiang <k1334755@students.katyisd.org>
2021-10-26 18:02:31 -0500

    .

--- Main.java
+++ Main.java
@@ -1,10 +1,12 @@
 public class Main{
     public static void main(String[] args){
-        var scan = new java.util.Scanner(System.in);
-        while(true){
-            System.out.print(">>> ");
-            if(Parsing.exec(scan.nextLine())) break;
-        }
-        scan.close();
+        // var scan = new java.util.Scanner(System.in);
+        // while(true){
+        //     System.out.print(">>> ");
+        //     if(Parsing.exec(scan.nextLine())) break;
+        // }
+        // scan.close();
+
+        System.out.println(new Value.Number(3.14159));
     }
 }
--- Parsing.java
+++ Parsing.java
@@ -2,13 +2,15 @@ import java.util.*;
 import java.util.regex.*;
 
 public class Parsing {
-    private static Pattern pattern = Pattern.compile("", 0);
+    public static Parser parser = new Parser();
+    public static void init(){
+
+    }
+
     public static boolean exec(String s){
         if(s.equals("quit")) return true;
 
-        for(MatchResult match : allMatches(pattern, s)){
-            
-        }
+
 
         System.out.println(s);
         return false;
@@ -42,11 +44,82 @@ public class Parsing {
     }
 }
 
+class Parser{
+
+}
+
 class Token{
-    private int LITERAL_TOKEN = 1;
-    private int PRE_UNARY_OPERATOR_TOKEN = 2;
-    private int POST_UNARY_OPERATOR_TOKEN = 4;
-    private int BINARY_OPERATOR_TOKEN = 8;
-    private int IDENTIFIER_TOKEN = 16;
-    
+    public final String name;
+    public final String value;
+
+    public Token(String name){
+        this.name = name;
+        this.value = null;
+    }
+    public Token(String name, String value){
+        this.name = name;
+        this.value = value;
+    }
+
+    public String toString(){
+        return "TOKEN<" + name + (value != null ? " " + value : "") + ">";
+    }
+}
+
+abstract class Value<T1>{
+    public final T1 value;
+    public Value(T1 value){this.value = value;}
+    public T1 value(){return this.value;}
+
+    public static class Number extends Value<Double>{
+        public Number(double value){
+            super(value);
+        }
+        public Number(Number n){
+            super(n.value());
+        }
+
+        public String toString(){
+            return value.toString();
+        }
+    }
+    public static class Vector extends Value<Number[]>{
+        public Vector(Number ...value){
+            super(value);
+        }
+        public Vector(double ...value){
+            super(new Number[value.length]);
+            int i = 0;
+            for(double v : value) this.value[i++] = new Number(v);
+        }
+        public Vector(Vector v){
+            super(v.value().clone());
+        }
+        public Vector(List<Number> L){
+            super((Number[]) L.value().clone());
+        }
+
+        public String toString(){
+            StringBuilder s = new StringBuilder("(" + value[0]);
+            for(int i = 1; i < value.length; i++) s.append(", " + value[i]);
+            return s.toString() + ")";
+        }
+    }
+    public static class List<T extends Value<?>> extends Value<T[]>{
+        public List(T[] value){
+            super(value);
+        }
+        public List(Vector v){
+            super((T[]) v.value().clone());
+        }
+        public List(List<?> L){
+            super((T[]) L.value().clone());
+        }
+
+        public String toString(){
+            StringBuilder s = new StringBuilder("[" + value[0]);
+            for(int i = 1; i < value.length; i++) s.append(", " + value[i]);
+            return s.toString() + "]";
+        }
+    }
 }
